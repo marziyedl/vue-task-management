9aae52f1c39874cf4ecb1147e817cbdd679cd045
m.dolati <marziye.dl@gmail.com>
2023-10-04 12:22:06 +0330

    feat: 🎸 show task detail

---
+++ src/views/TaskDetailView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import useFetchById from "@/composable/useFetchById";
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import Badge from "@/components/Badge.vue";
+
+const { getById } = useFetchById<{ id: string }>("tasks");
+const route = useRouter();
+const taskDetail = ref(getById(route.currentRoute.value.params.id as string));
+
+</script>
+
+<template>
+    <section class="w-full p-3 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+        <div class="flex justify-between">
+            <Badge :type="taskDetail?.status || 'done'" />
+            <h5 class="text-lg">{{ taskDetail?.title }}</h5>
+        </div>
+        <p class="box-content mt-2">{{ taskDetail?.content }}</p>
+    </section>
+</template>
