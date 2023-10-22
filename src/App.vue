f36e911fc043b0be75df8e009c021b340103fbe1
m.dolati <marziye.dl@gmail.com>
2023-10-22 10:52:43 +0330

    feat: 🎸 implemment layout:header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import "./style.css"
-
-// const tasks = reactive(useFetchDataFromLocalStorage())
-// console.log(tasks);
+import Header from "@/layout/Header.vue"
+import Footer from "@/layout/Footer.vue"
 </script>
 
 <template>
+  <Header></Header>
   <router-view></router-view>
+  <Footer></Footer>
 </template>
---
+++ src/layout/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="bg-white py-2 md:py-4">
+    <div class="container px-4 mx-auto md:flex md:items-center">
+
+      <div class="flex justify-between items-center">
+        <router-link to="/" href="#" class="font-bold text-xl text-indigo-600">MARZI</router-link>
+        <button
+          class="border border-solid border-gray-600 px-3 py-1 rounded text-gray-600 opacity-50 hover:opacity-75 md:hidden"
+          id="navbar-toggle">
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+
+      <div class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+        <router-link to="/create" class="p-2 lg:px-4 md:mx-2 text-white rounded bg-indigo-600">CREATE</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
